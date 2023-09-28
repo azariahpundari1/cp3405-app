efc479c35f2b0e311d695d10aa6a3870165baa0d
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-09-28 12:05:57 +1000

    Add segment control view

--- lib/Student/HomePageStudent.dart
+++ lib/Student/HomePageStudent.dart
@@ -1,14 +1,53 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables
 
-class HomePageStudent extends StatelessWidget {
+import 'package:flutter/cupertino.dart';
+
+class HomePageStudent extends StatefulWidget {
+  const HomePageStudent({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageStudentState createState() => _HomePageStudentState();
+}
+
+class _HomePageStudentState extends State<HomePageStudent> {
+  int _selectedSegment = 0;
+
+  // Create a map to associate segment values with their text labels
+  final Map<int, String> segmentText = {
+    0: 'Yesterday',
+    1: 'Today',
+    2: 'Tomorrow',
+  };
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen Student'),
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Cupertino Segmented Control Example'),
       ),
-      body: Center(
-        child: Text('Home Screen Content'),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CupertinoSegmentedControl<int>(
+              children: {
+                0: const Text('Yesterday'),
+                1: const Text('Today'),
+                2: const Text('Tomorrow'),
+              },
+              groupValue: _selectedSegment,
+              onValueChanged: (value) {
+                setState(() {
+                  _selectedSegment = value;
+                });
+              },
+            ),
+            const SizedBox(height: 20),
+            Text(
+                'Selected Segment: ${segmentText[_selectedSegment]}'), // Display the selected text
+          ],
+        ),
       ),
     );
   }
