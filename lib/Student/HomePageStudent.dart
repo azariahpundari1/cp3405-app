361ba40548a078910af305a576cd1797d135a7df
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-09-28 22:02:10 +1000

    Add calander schedule view

--- lib/Student/HomePageStudent.dart
+++ lib/Student/HomePageStudent.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class HomePageStudent extends StatefulWidget {
   const HomePageStudent({super.key});
@@ -24,7 +26,7 @@ class _HomePageStudentState extends State<HomePageStudent> {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(
-        middle: Text('Cupertino Segmented Control Example'),
+        middle: Text('Main Screen'),
       ),
       child: Center(
         child: Column(
@@ -43,14 +45,76 @@ class _HomePageStudentState extends State<HomePageStudent> {
                 });
               },
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-                'Selected Segment: ${segmentText[_selectedSegment]}'), // Display the selected text
+            const SizedBox(height: 20),
+
+            // second column
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // Display calendar
+              children: <Widget>[
+                SfCalendar(
+                  view: CalendarView.schedule,
+                  dataSource: _getDataSource(),
+                )
+              ],
+            )
           ],
         ),
       ),
     );
+  }
+
+  // Display appointments
+  _DataSource _getDataSource() {
+    final List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: -2)),
+      endTime: DateTime.now().add(const Duration(hours: -1)),
+      subject: 'Science',
+      color: Colors.red,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 2)),
+      endTime: DateTime.now().add(Duration(hours: 1)),
+      subject: 'Math',
+      color: Color(0xFFf527318),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 2)),
+      endTime: DateTime.now().add(Duration(hours: 1)),
+      subject: 'English',
+      color: Color(0xFFfb21f66),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 2)),
+      endTime: DateTime.now().add(Duration(hours: 1)),
+      subject: 'History',
+      color: Color(0xFFf3282b8),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(Duration(hours: 2)),
+      endTime: DateTime.now().add(Duration(hours: 1)),
+      subject: 'Physics',
+      color: Color(0xFFf2a7886),
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 1)),
+      subject: 'Chemistry',
+      color: Colors.lightBlueAccent,
+    ));
+    appointments.add(Appointment(
+      startTime: DateTime.now().add(const Duration(hours: 2)),
+      endTime: DateTime.now().add(const Duration(hours: 1)),
+      subject: 'Biology',
+      color: Colors.amber,
+    ));
+    return _DataSource(appointments);
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
