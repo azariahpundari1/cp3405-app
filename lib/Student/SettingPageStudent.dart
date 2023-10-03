1e896a51e540bf1ce28a5d37c42b2809b9a75b22
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-10-03 23:39:18 +1000

    Remove unused method

--- lib/Student/SettingPageStudent.dart
+++ lib/Student/SettingPageStudent.dart
@@ -1,28 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SettingsPageStudent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SettingsList(
-      sections: [
-        SettingsSection(
-          title: Text('Common'),
-          tiles: <SettingsTile>[
-            SettingsTile.navigation(
-              leading: Icon(Icons.language),
-              title: Text('Language'),
-              value: Text('English'),
-            ),
-            SettingsTile.switchTile(
-              onToggle: (value) {},
-              initialValue: true,
-              leading: Icon(Icons.format_paint),
-              title: Text('Enable custom theme'),
-            ),
-          ],
-        ),
-      ],
+    return MaterialApp(
+      title: "Settings Example",
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: SettingsPage(),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _isDarkModeEnabled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Settings'),
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.language),
+                title: const Text('Language'),
+                value: const Text('English'),
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {
+                  setState(() {
+                    _isDarkModeEnabled = value;
+                  });
+                },
+                initialValue: _isDarkModeEnabled,
+                leading: const Icon(Icons.format_paint),
+                title: const Text('Enable Dark Mode'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
