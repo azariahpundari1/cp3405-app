adda7e17653c5455e07df1b342a87e6dd3a05fea
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2023-10-03 17:34:27 +1000

    Add basic setting components

--- lib/SettingPage.dart
+++ lib/SettingPage.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-      ),
-      body: Center(
-        child: Text('Settings still in development'),
-      ),
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          title: Text('Common'),
+          tiles: <SettingsTile>[
+            SettingsTile.navigation(
+              leading: Icon(Icons.language),
+              title: Text('Language'),
+              value: Text('English'),
+            ),
+            SettingsTile.switchTile(
+              onToggle: (value) {},
+              initialValue: true,
+              leading: Icon(Icons.format_paint),
+              title: Text('Enable custom theme'),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
